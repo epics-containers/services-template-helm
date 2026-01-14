1a554f4c91508500327323730cc86af9e00e51f9
Ollie Copping <oliver.copping@diamond.ac.uk>
2026-01-14 13:03:15 +0000

    Make sure values.yaml is included in the CI
    We also need to make sure it is not included in the list of changed
    services (otherwise CI will run for it)

--- template/ci_verify.sh
+++ template/ci_verify.sh
@@ -50,9 +50,10 @@ else
   DIFF_BASE="${CI_COMMIT_BEFORE_SHA}"
 fi
 
-# Get changed services
+# Get changed services (excluding values.yaml)
 CHANGED_SERVICES=$(git diff --name-only "$DIFF_BASE" "$CI_COMMIT_SHA" \
   | grep '^services/' \
+  | grep -v "values.yaml" \
   | cut -d/ -f2 \
   | sort -u)
 
@@ -60,6 +61,9 @@ if [[ -z "$CHANGED_SERVICES" ]]; then
   echo "No services changed. Exiting."
   exit 0
 fi
+
+# Need to make sure values.yaml is included in the ci
+cp -L "${ROOT}/services/values.yaml" "${ROOT}/.ci_work/"
 
 # copy only the changed services to a temporary location to avoid dirtying the repo
 for svc in $CHANGED_SERVICES; do
